ce3758044bbd67ea183318c203a1464b0c461bcc
kevinbuch <kevaustinbuch@gmail.com>
2013-09-26 15:15:18 -0500

    refactored strategy spec

--- spec/strategies_spec.rb
+++ spec/strategies_spec.rb
@@ -18,51 +18,54 @@ describe Strategies::TicTacToe::Human do
 end
 
 describe Strategies::TicTacToe::KevinsAI do
-  let(:game) { TicTacToe.new(@board) }
+
+  def game(board)
+    TicTacToe.new(board)
+  end
 
   describe '#next_move' do
     context 'there is a winning move' do
       it 'takes the winning space' do
-        @board = 'XX-------'
-        expect(subject.next_move(game, 'X')).to eq(2)
+        board = 'XX-------'
+        expect(subject.next_move(game(board), 'X')).to eq(2)
       end
     end
 
     context 'there is a defensive move' do
       it 'blocks the winning space' do
-        @board = 'OO-------'
-        expect(subject.next_move(game, 'X')).to eq(2)
+        board = 'OO-------'
+        expect(subject.next_move(game(board), 'X')).to eq(2)
       end
     end
 
     context 'there is a winning and defensive move' do
       it 'takes the winning space' do
-        @board = 'XX-OO----'
-        expect(subject.next_move(game, 'X')).to eq(2)
+        board = 'XX-OO----'
+        expect(subject.next_move(game(board), 'X')).to eq(2)
       end
     end
 
     context 'it is the first move' do
       it 'takes an edge space' do
-        @board = '---------'
-        expect([1, 3, 5, 7]).to include(subject.next_move(game, 'X'))
+        board = '---------'
+        expect([1, 3, 5, 7]).to include(subject.next_move(game(board), 'X'))
       end
     end
 
     context 'it must make a strategic move' do
       it 'takes a line with one opponent character' do
-        @board = '-X-O-----'
-        expect(subject.next_move(game, 'X')).to eq(0)
+        board = '-X-O-----'
+        expect(subject.next_move(game(board), 'X')).to eq(0)
       end
 
       it 'takes an edge space over a corner space if opposite corners are taken' do
-        @board = 'O---X---O'
-        expect(subject.next_move(game, 'X')).to eq(5)
+        board = 'O---X---O'
+        expect(subject.next_move(game(board), 'X')).to eq(5)
       end
 
       it 'takes a corner space if the opponent took the middle space' do
-        @board = '----O----'
-        expect(subject.next_move(game, 'X')).to eq(2)
+        board = '----O----'
+        expect(subject.next_move(game(board), 'X')).to eq(2)
       end
     end
   end
