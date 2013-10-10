d01de6c2108e284b97f28b7b7bb4233aed2cd832
kevinbuch <kevaustinbuch@gmail.com>
2013-10-10 14:48:43 -0500

    changed to use strategy classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,10 @@
 require_relative 'app/game'
-require_relative 'app/player'
 require_relative 'app/round'
 require_relative 'app/strategies'
 require_relative 'app/view'
 
-human = Player.new('O', Strategies::TicTacToe::Human)
-ai = Player.new('X', Strategies::TicTacToe::KevinsAI)
+human = Strategies::TicTacToe::Human.new('O')
+ai = Strategies::TicTacToe::KevinsAI.new('X')
 game = TicTacToe.new
 view = TicTacToeView.new
 round = Round.new(game, view, human, ai)
